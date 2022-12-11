d83f9a7671651fde4c3cb5b01c78420182b8770c
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2022-12-11 01:57:57 +0100

    Added Practicum_9B

---
+++ src/Practica/Practicum_9B/userinterface/BoekingenController.java
@@ -0,0 +1,43 @@
+package Practica.Practicum_9B.userinterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class BoekingenController {
+
+    @FXML
+    private Button ButtonBoek;
+
+    @FXML
+    private Button ButtonReset;
+
+    @FXML
+    private ComboBox<?> ComboBoxKamerType;
+
+    @FXML
+    private DatePicker DatePickerAankomstDatum;
+
+    @FXML
+    private DatePicker DatePickerVertrekDatum;
+
+    @FXML
+    private TextField textBoxAdres;
+
+    @FXML
+    private TextField textBoxNaam;
+
+    @FXML
+    void ButtonBoek_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ButtonReset_Click(ActionEvent event) {
+
+    }
+
+}
